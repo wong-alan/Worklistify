35c2f9f93d41edef41f03a3430eabb6873bc053d
Jack <jackubc2013@gmail.com>
2017-03-19 10:27:41 -0700

    Added basic api for courses

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const fs = require('fs');
-const sqlite = require('sql.js');
-
-const filebuffer = fs.readFileSync('db/usda-nnd.sqlite3');
-
-const db = new sqlite.Database(filebuffer);
 
 const app = express();
 
@@ -15,54 +10,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-const COLUMNS = [
-  'carbohydrate_g',
-  'protein_g',
-  'fa_sat_g',
-  'fa_mono_g',
-  'fa_poly_g',
-  'kcal',
-  'description',
-];
-app.get('/api/food', (req, res) => {
-  const param = req.query.q;
+app.get('/api/courses', (req, res) => {
+  var c1 = req.query.c1;
+  var c2 = req.query.c2;
+  var c3 = req.query.c3;
+  var c4 = req.query.c4;
+  var c5 = req.query.c5;
+  var c6 = req.query.c6;
+  var c7 = req.query.c7;
 
-  if (!param) {
-    res.json({
-      error: 'Missing required parameter `q`',
-    });
-    return;
-  }
-
-  // WARNING: Not for production use! The following statement
-  // is not protected against SQL injections.
-  const r = db.exec(`
-    select ${COLUMNS.join(', ')} from entries
-    where description like '%${param}%'
-    limit 100
-  `);
-
-  if (r[0]) {
-    res.json(
-      r[0].values.map((entry) => {
-        const e = {};
-        COLUMNS.forEach((c, idx) => {
-          // combine fat columns
-          if (c.match(/^fa_/)) {
-            e.fat_g = e.fat_g || 0.0;
-            e.fat_g = (
-              parseFloat(e.fat_g, 10) + parseFloat(entry[idx], 10)
-            ).toFixed(2);
-          } else {
-            e[c] = entry[idx];
-          }
-        });
-        return e;
-      }),
-    );
-  } else {
-    res.json([]);
-  }
+  res.json([c1, c2, c3, c4, c5, c6, c7]);
 });
 
 app.listen(app.get('port'), () => {
